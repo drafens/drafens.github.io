19aa6124937cb7e36eeee37aa68d0d815e9265ec
drafens <1774087313@qq.com>
2019-06-01 23:32:18 +0800

    add about-me and thanks pages

--- js/function.js
+++ js/function.js
@@ -1,22 +1,36 @@
-<!-- 随机banner-image -->
+// 随机banner-image
 window.onload = loadBanner;
 const banner_size = 13;
 function loadBanner() {
     document.getElementById("banner-box").style = "background-image: url('images/banner/banner(" + Math.floor(Math.random() * banner_size + 1) + ").jpg')";
 }
 
-<!-- 单击回顶部 -->
+// 单击回顶部
 $('#lolijump').click(function() {
     $('html, body').animate({
         scrollTop: 0
     },500);
 });
 
+// 调整live2D大小
 const canvas = document.getElementById("live2d");
 if (window.matchMedia("(max-width: 768px)").matches) {
-    canvas.width = "140";
-    canvas.height = "125";
+    canvas.width = "112";
+    canvas.height = "100";
 }else{
     canvas.width = "280";
     canvas.height = "250";
+}
+
+//切换主界面
+const divs = ["home", "about-me", "thanks"];
+function switchDivs(tag) {
+    let div;
+    for (i of divs) {
+        div = document.getElementById(i);
+        if (tag === i)
+            div.style.display = "block";
+        else
+            div.style.display = "none";
+    }
 }
