7c121bf5d7bf103f4eed55133ee307fdf031b437
drafens <1774087313@qq.com>
2019-06-02 17:22:41 +0800

    fix the liv2d blur in mobile

--- js/function.js
+++ js/function.js
@@ -13,14 +13,16 @@ $('#lolijump').click(function() {
 });
 
 // 调整live2D大小
-const canvas = document.getElementById("live2d");
-if (window.matchMedia("(max-width: 768px)").matches) {
-    canvas.width = "112";
-    canvas.height = "100";
-}else{
-    canvas.width = "280";
-    canvas.height = "250";
-}
+// const canvas = document.getElementById("live2d");
+// if (window.matchMedia("(max-width: 768px)").matches) {
+//     canvas.width = "280";
+//     canvas.height = "250";
+//     canvas.style="width:112px; height:100px"
+// }else{
+//     canvas.width = "280";
+//     canvas.height = "250";
+//     canvas.style="width:280px; height:250px"
+// }
 
 //切换主界面div
 const divs = ["home", "about-me", "thanks"];
