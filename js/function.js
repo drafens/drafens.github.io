7ee81524c8faeee966242e5930501bd2981247e9
drafens <1774087313@qq.com>
2019-06-03 11:08:21 +0800

    add 404-not-found page

--- js/function.js
+++ js/function.js
@@ -12,18 +12,6 @@ $('#lolijump').click(function() {
     },500);
 });
 
-// 调整live2D大小
-// const canvas = document.getElementById("live2d");
-// if (window.matchMedia("(max-width: 768px)").matches) {
-//     canvas.width = "280";
-//     canvas.height = "250";
-//     canvas.style="width:112px; height:100px"
-// }else{
-//     canvas.width = "280";
-//     canvas.height = "250";
-//     canvas.style="width:280px; height:250px"
-// }
-
 //切换主界面div
 const divs = ["home", "about-me", "thanks"];
 function switchDivs(tag) {
