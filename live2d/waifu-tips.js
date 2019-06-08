62188904b6f301194769cdd2d590efedb80333bb
drafens <1774087313@qq.com>
2019-06-08 13:06:26 +0800

    rich live2d features

--- live2d/waifu-tips.js
+++ live2d/waifu-tips.js
@@ -1,6 +1,6 @@
 function render(template, context) {
 
-    var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
+    const tokenReg = /(\\)?{([^{}\\]+)(\\)?}/g;
 
     return template.replace(tokenReg, function (word, slash1, token, slash2) {
         if (slash1 || slash2) {  
@@ -58,6 +58,7 @@ $.ajax({
     }
 });
 
+let waifu_tips;
 (function (){
     var text;
     if(document.referrer !== ''){
@@ -65,15 +66,15 @@ $.ajax({
         referrer.href = document.referrer;
         text = 'Hello! 来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友';
         var domain = referrer.hostname.split('.')[1];
-        if (domain == 'baidu') {
+        if (domain === 'baidu') {
             text = 'Hello! 来自 百度搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&wd=')[1].split('&')[0] + '</span> 找到的我吗？';
-        }else if (domain == 'so') {
+        }else if (domain === 'so') {
             text = 'Hello! 来自 360搜索 的朋友<br>你是搜索 <span style="color:#0099cc;">' + referrer.search.split('&q=')[1].split('&')[0] + '</span> 找到的我吗？';
-        }else if (domain == 'google') {
+        }else if (domain === 'google') {
             text = 'Hello! 来自 谷歌搜索 的朋友<br>欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
         }
     }else {
-        if (window.location.href == './index.html') { //如果是主页
+        // if (window.location.href === 'https://drafens.coding.me/') { //如果是主页
             var now = (new Date()).getHours();
             if (now > 23 || now <= 5) {
                 text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛';
@@ -94,31 +95,33 @@ $.ajax({
             } else {
                 text = '嗨~ 快来逗我玩吧！';
             }
-        }else {
-            text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
-        }
+        // }else {
+        //     text = '欢迎阅读<span style="color:#0099cc;">『' + document.title.split(' - ')[0] + '』</span>';
+        // }
     }
+    waifu_tips = $('.waifu-tips');
     showMessage(text, 6000);
 })();
 
-window.setInterval(showHitokoto,30000);
+window.setInterval(showHitokoto,60000);
 
 function showHitokoto(){
-    $.getJSON('https://api.imjad.cn/hitokoto/?cat=&charset=utf-8&length=28&encode=json',function(result){
+    $.getJSON('https://v1.hitokoto.cn/?cat=&charset=utf-8&length=28&encode=json',function(result){
         showMessage(result.hitokoto, 5000);
     });
 }
 
 function showMessage(text, timeout){
-    if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
+    if (Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1) - 1];
     console.log(text);
-    $('.waifu-tips').stop();
-    $('.waifu-tips').html(text).fadeTo(200, 1);
+    waifu_tips.stop(false,true).animate();//上一动画直接到达末态
+    waifu_tips.html(text).fadeTo(200, 1);
     if (timeout === null) timeout = 5000;
     hideMessage(timeout);
+
 }
 function hideMessage(timeout){
-    $('.waifu-tips').stop().css('opacity',1);
+    waifu_tips.stop().css('opacity',1);
     if (timeout === null) timeout = 5000;
-    $('.waifu-tips').delay(timeout).fadeTo(200, 0);
+    waifu_tips.delay(timeout).fadeTo(200, 0);
 }
